dc7deaf44ce5b831c6e78133b08f3d2ec06ef968
Aiman Nabeel <aiman.nabeel@gmail.com>
2020-09-09 17:04:49 +0100

    Fix toolbar

--- app/src/main/java/com/example/composablecookbookapp/MainActivity.kt
+++ app/src/main/java/com/example/composablecookbookapp/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.composablecookbookapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 import com.example.composablecookbookapp.ui.ComposableCookbookAppTheme
@@ -16,7 +22,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                RecipeList(recipes = defaultRecipes)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "ComposableCookBook") },
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.White,
+                            elevation = 12.dp
+                        )
+                    }, bodyContent = {
+                        RecipeList(recipes = defaultRecipes)
+                    }
+                )
             }
         }
     }
@@ -27,10 +44,18 @@ class MainActivity : AppCompatActivity() {
 fun DefaultPreview() {
     ComposableCookbookAppTheme {
         MaterialTheme {
-            TopAppBar(
-                title = {Text(text = "ComposableCookBook")}
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text(text = "ComposableCookBook") },
+                        backgroundColor = Color.Blue,
+                        contentColor = Color.White,
+                        elevation = 12.dp
+                    )
+                }, bodyContent = {
+                    RecipeList(recipes = defaultRecipes)
+                }
             )
-            RecipeList(recipes = defaultRecipes)
         }
     }
 }
